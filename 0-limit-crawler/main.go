6b610b00c1e2c22d309ee71b69a2ed7a9f5afa51
Molodtsov Yaroslav <105973491+passwordhash@users.noreply.github.com>
2025-05-28 18:52:58 +0300

    🍕 Добавлено решение первого задания: семафор + задержка

--- 0-limit-crawler/main.go
+++ 0-limit-crawler/main.go
@@ -12,18 +12,28 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+// Дан краулер (модифицированный из тура Go), который запрашивает страницы
+// чрезмерно часто. Однако мы не хотим слишком нагружать веб-сервер
+// сильно. Ваша задача - изменить код таким образом, чтобы он ограничивался не более чем
+// одной страницы в секунду, сохраняя при этом параллелизм (другими словами,
+// Crawl() должен вызываться параллельно)
 
 // Crawl uses `fetcher` from the `mockfetcher.go` file to imitate a
 // real crawler. It crawls until the maximum depth has reached.
-func Crawl(url string, depth int, wg *sync.WaitGroup) {
+func Crawl(url string, depth int, wg *sync.WaitGroup, token chan struct{}) {
 	defer wg.Done()
 
 	if depth <= 0 {
 		return
 	}
 
+	token <- struct{}{}
 	body, urls, err := fetcher.Fetch(url)
+	time.Sleep(1 * time.Second)
+	<-token
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -35,14 +45,15 @@ func Crawl(url string, depth int, wg *sync.WaitGroup) {
 	for _, u := range urls {
 		// Do not remove the `go` keyword, as Crawl() must be
 		// called concurrently
-		go Crawl(u, depth-1, wg)
+		go Crawl(u, depth-1, wg, token)
 	}
 }
 
 func main() {
 	var wg sync.WaitGroup
+	token := make(chan struct{}, 1)
 
 	wg.Add(1)
-	Crawl("http://golang.org/", 4, &wg)
+	Crawl("http://golang.org/", 4, &wg, token)
 	wg.Wait()
 }
