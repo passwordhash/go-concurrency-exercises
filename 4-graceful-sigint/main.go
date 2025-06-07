2ae21e9287a6283e52fecef8f94f5cb95630cbd0
Molodtsov Yaroslav <105973491+passwordhash@users.noreply.github.com>
2025-06-07 19:27:15 +0300

    Добавлено решение четвертого задания

--- 4-graceful-sigint/main.go
+++ 4-graceful-sigint/main.go
@@ -2,21 +2,36 @@
 //
 // Given is a mock process which runs indefinitely and blocks the
 // program. Right now the only way to stop the program is to send a
-// SIGINT (Ctrl-C). Killing a process like that is not graceful, so we
+// stopINT (Ctrl-C). Killing a process like that is not graceful, so we
 // want to try to gracefully stop the process first.
 //
 // Change the program to do the following:
-//   1. On SIGINT try to gracefully stop the process using
+//   1. On stopINT try to gracefully stop the process using
 //          `proc.Stop()`
-//   2. If SIGINT is called again, just kill the program (last resort)
+//   2. If stopINT is called again, just kill the program (last resort)
 //
 
 package main
 
+import (
+	"os"
+	"os/signal"
+)
+
 func main() {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
 	// Create a process
 	proc := MockProcess{}
 
 	// Run the process (blocking)
-	proc.Run()
+	go proc.Run()
+
+	<-stop
+	go proc.Stop()
+
+	signal.Reset(os.Interrupt)
+
+	select {}
 }
